5e1a5177f9fb49587d1e2fa8f1d4901c16882f08
Jon Nordby <jononor@gmail.com>
2019-04-25 00:33:27 +0200

    convtrees: Restructure code a bit

--- convtrees/convtrees.py
+++ convtrees/convtrees.py
@@ -16,6 +16,27 @@ from sklearn.cluster import KMeans
 
 from spherecluster import SphericalKMeans
 
+def plot_codebook(codebook):
+    shape = codebook[0].shape
+
+    n_rows = 5
+    cols, rows = math.ceil(len(codebook)/n_rows), n_rows
+
+    fig, axs = plt.subplots(rows, cols, figsize=(4*3,4))
+
+    fig.patch.set_facecolor('grey')
+    for ax in axs.flatten():
+        ax.set_visible(False)
+
+    for ax, kernel in zip(axs.flatten(), codebook):
+        ax.set_visible(True)
+        ax.imshow(kernel, cmap='gray')
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+
+    fig.tight_layout()
+
+    return fig
 
 
 def random_kernels(N=20, K=3):
@@ -23,22 +44,15 @@ def random_kernels(N=20, K=3):
     k = r.reshape(-1, K, K)
     return k
 
-def random_locations(width, height, codebook, n_locations=10, n_kernels=1):
-    # where to sample
-    xs = numpy.random.randint(0, width, n_locations)
-    ys = numpy.random.randint(0, height, n_locations)
-
-    # which kernels to use
-    k_idx = numpy.random.randint(0, len(codebook), n_locations*n_kernels)
-    return xs, ys, k_idx 
-
-def random_convs(img, codebook, kernels, xs, ys, K):
+def convolve(img, codebook, kernels, ls, xs, ys, K):
     
     features = numpy.zeros(shape=(len(kernels),))
 
     # TODO: vectorize
-    for i, (k, x, y) in enumerate(zip(kernels, xs, ys)):
+    for i, (l, k, x, y) in enumerate(zip(ls, kernels, xs, ys)):
+        #print('ii', i//5, k, codebook.shape)
         kernel = codebook[k]
+        #kernel = numpy.zeros(shape=(K,K))
 
         xmax = min(img.shape[0],x+K)
         ymax = min(img.shape[1],y+K)
@@ -49,23 +63,31 @@ def random_convs(img, codebook, kernels, xs, ys, K):
 
     return numpy.stack(features)        
 
-def sample_patches(imgs, K=3, n_patches=10):
-    N = len(imgs) * n_patches    
-    shape = imgs[0].shape
+def locations_random_full(shape, N, K=None):
+    xs = numpy.random.randint(0, shape[0], N)
+    ys = numpy.random.randint(0, shape[0], N)
+    return xs, ys
 
+def locations_random_valid(shape, N, K=3):
     xs = numpy.random.randint(0, shape[0]-K, N)
     ys = numpy.random.randint(0, shape[1]-K, N)
+    return xs, ys
 
-    # TODO: vectorize
-    out = []
-    for i, x, y in zip(range(len(imgs)), xs, ys):
-        xmax = x+K
-        ymax = y+K
-        patch = imgs[i, x:xmax, y:ymax]
-        out.append(patch)
 
-    return numpy.stack(out)
+def sample_patches(imgs, locations, K, n_patches=100):
+    N = len(imgs) * n_patches    
 
+    xs, ys = locations
+    assert len(xs) == len(ys)
+    out = numpy.zeros(shape=(len(xs), n_patches, K, K))
+
+    img_range = numpy.array(range(0, len(imgs)))
+    for l, (x, y) in enumerate(zip(xs, ys)):
+        subset = numpy.random.choice(img_range, size=n_patches)
+        patch = imgs[subset, x:x+K, y:y+K]
+        out[l] = patch
+
+    return out
 
 def kmeans_codebook(patches, k=30):
     shape = patches[0].shape
@@ -81,30 +103,7 @@ def kmeans_codebook(patches, k=30):
     codebook = est.cluster_centers_.reshape(-1, shape[0], shape[1])
     return codebook
 
-def plot_codebook(codebook):
-    shape = codebook[0].shape
 
-    n_rows = 5
-    cols, rows = math.ceil(len(codebook)/n_rows), n_rows
-    print('nn', cols, rows, cols*rows)
-
-
-    fig, axs = plt.subplots(rows, cols, figsize=(4*3,4))
-
-    fig.patch.set_facecolor('grey')
-    for ax in axs.flatten():
-        ax.set_visible(False)
-
-    for ax, kernel in zip(axs.flatten(), codebook):
-        print('c', kernel)
-        ax.set_visible(True)
-        ax.imshow(kernel, cmap='gray')
-        ax.axes.get_xaxis().set_visible(False)
-        ax.axes.get_yaxis().set_visible(False)
-
-    fig.tight_layout()
-
-    return fig
 
 def evaluate_mnist():
 
@@ -112,38 +111,64 @@ def evaluate_mnist():
     test_x, test_y  = mnist.test_images(), mnist.test_labels()
 
     K=3
-    codebook_size = 50
-    locations = 100
-    kernels = codebook_size//5
+    codebook_size = 30
+    n_locations = 20
+    n_kernels = 5
+    input_shape = (28, 28)
 
-    random_codebook = random_kernels(K=K, N=codebook_size)
+    #random_codebook = random_kernels(K=K, N=codebook_size)
     #print('rr code', codebook.shape)
     print('Creating codebook')
-    pp = sample_patches(train_x, K=K)
-    codebook = kmeans_codebook(pp, k=codebook_size)
-    print('kmeans code', codebook.shape)
+
+    locations = locations_random_valid(input_shape, N=n_locations, K=K)
+    assert len(locations[0]) == n_locations, (len(locations[0]), n_locations)
+    
+    loc_patches = sample_patches(train_x, locations, K=K)
+    all_patches = loc_patches.reshape(-1, K, K)
+
+    print('pp', loc_patches.shape, all_patches.shape)
+
+    codebook = kmeans_codebook(all_patches, k=codebook_size)
+    print('kmeans global codebook', codebook.shape)
+
+    loc_codebooks = numpy.array([ kmeans_codebook(p, k=codebook_size) for p in loc_patches ])
+    print('loc codebooks', loc_codebooks.shape)
 
     fig = plot_codebook(codebook)
     fig.savefig('codebook.png', facecolor=fig.get_facecolor())
 
-    input_shape = (28, 28)
 
     input_area = numpy.array(input_shape).prod()
-    sample_area = K*K*locations
-    coverage = sample_area / input_area 
+    sample_area = K*K*n_locations
+    coverage = sample_area / input_area
+    print('cc', sample_area, input_area)
     # TODO: could calculate effective sampled area
     print('Coverage: {:d}% {:d}px/{:d}px'.format(int(coverage*100), input_area, sample_area))
 
-    xs, ys, ks = random_locations(28, 28, codebook, n_locations=locations, n_kernels=kernels)
+    xs, ys = locations
+    xs = numpy.repeat(xs, n_kernels)
+    ys = numpy.repeat(ys, n_kernels)
+    ls = list(range(n_locations))
+    ls = numpy.repeat(ls, n_kernels)
+
+    # TODO: try local codebooks
+    ks = numpy.random.randint(0, codebook_size, len(xs))
+
+    assert len(ks) == len(xs), (len(ks), len(xs))
+    assert len(ls) == len(xs), (len(ls), len(xs))
+
+#    ks = numpy.flatten([ numpy.random.randint(0, len(cc), size=n_kernels) for cc in loc_codebooks ])
+
 
     def transform(imgs):
-        f = [ random_convs(i, codebook, ks, xs, ys, K=K) for i in imgs ]
+        f = [ convolve(i, codebook, ks, ls, xs, ys, K=K) for i in imgs ]
         f = numpy.array(f)
         return f
 
     print('Precomputing features')
     train_x = transform(train_x)
     test_x = transform(test_x)
+    print('Feat', train_x.shape)
 
     clf = RandomForestClassifier(n_estimators=100, min_samples_leaf=1e-6)
     params = {
