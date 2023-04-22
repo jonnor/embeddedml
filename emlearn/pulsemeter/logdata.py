d20d3f6369788cd0d7df5d7575a6c3050fa5b073
Jon Nordby <jononor@gmail.com>
2023-04-22 11:39:38 +0200

    pulsemeter: Tool for saving data from serial

---
+++ emlearn/pulsemeter/logdata.py
@@ -0,0 +1,45 @@
+import structlog
+import serial
+
+log = structlog.get_logger()
+
+def parse():
+    import argparse
+    parser = argparse.ArgumentParser(description='')
+
+    parser.add_argument('--out', metavar='FILE', type=str, default='',
+                        help='Where to put output')
+
+    parser.add_argument('--port', metavar='FILE', type=str, default='/dev/ttyACM0',
+                        help='Serial port to read from')
+    parser.add_argument('--baudrate', metavar='NUMBER', type=int, default=115200,
+                        help='Baudrate of serial')
+    parser.add_argument('--timeout', type=float, default=0.1,
+                        help='Max time between reads')
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = parse()
+
+    baud = args.baudrate
+    timeout = args.timeout
+    out = args.out
+    port = args.port
+
+    with serial.Serial(port, baud, timeout=timeout) as ser:
+        with open(out, 'wb+') as outfile:
+
+            log.info('files-opened', serial=port, output=out)
+
+            while True:
+                line = ser.readline()
+                if line:
+                    outfile.write(line)
+
+if __name__ == '__main__':
+    main()
+
