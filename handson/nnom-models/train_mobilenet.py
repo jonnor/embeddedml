5168121a46b45c025f78c37a20ca109f652b3d8d
Jon Nordby <jononor@gmail.com>
2024-04-07 15:38:35 +0200

    nnom-models: Try to get MobileNet working
    Currently fails to generate a model
    
        File "../nnom-models/auto_test/venv/lib/python3.11/site-packages/nnom/nnom.py", line 731, in quantize_weights
          f.write('#define ' + layer.name.upper() + '_BIAS_LSHIFT '+to_cstyle(bias_shift) +'\n\n')
                                                                            ^^^^^^^^^^
       UnboundLocalError: cannot access local variable 'bias_shift' where it is not associated with a value
    
    MobileNet does not use bias for Conv layers. Currently that is not supported in NNoM,
    and might be the reason of this error.
    
    Ref https://github.com/majianjia/nnom/issues/80

--- handson/nnom-models/train_mobilenet.py
+++ handson/nnom-models/train_mobilenet.py
@@ -1,30 +1,119 @@
 
 import numpy
 import nnom
-import keras.applications
+from keras.applications import MobileNet
+import keras
 
-print('Keras version', keras.__version__)
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.models import load_model, save_model
+import tensorflow as tf
 
-model = keras.applications.MobileNet(
-    input_shape=(96, 96, 1),
-    alpha=0.25,
-    depth_multiplier=1,
-    dropout=0.001,
-    include_top=True,
-    weights=None,
-    pooling=None,
-    classes=521,
-    classifier_activation="softmax",
-)
+save_dir = 'keras_mnist_trained_model.h5'
 
-#model.summary()
+def build_model(width=96, height=96, classes=521, channels=1, alpha=0.25):
 
-#print(nnom.__path__)
-print(dir(nnom))
+    model = MobileNet(
+        input_shape=(width, height, channels),
+        alpha=alpha,
+        depth_multiplier=1,
+        dropout=0.001,
+        include_top=True,
+        weights=None,
+        pooling=None,
+        classes=classes,
+        classifier_activation="softmax",
+    )
 
-out_path = "kws_weights.h"
-data = numpy.random.rand(100, 96, 96, 1)
-nnom.generate_model(model, data, name=out_path)
+    return model
+
+def train(model, x_train, y_train, x_test, y_test, batch_size=64, epochs=50):
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    history = model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              verbose=2,
+              validation_data=(x_test, y_test),
+              shuffle=False)
+
+    return history
+
+def generate_c_model(x_test, y_test, save_dir, out_path = "weights.h"):
+    # -------- generate weights.h (NNoM model) ----------
+    # get the best model
+    model = load_model(save_dir)
+
+    # only use 1000 for test
+    x_test = x_test[:1000]
+    y_test = y_test[:1000]
+    # generate binary dataset for NNoM validation, 0~1 -> 0~127, q7
+    nnom.generate_test_bin(x_test*127, y_test, name='test_data.bin')
+
+    # evaluate in Keras (for comparision)
+    scores = nnom.evaluate_model(model, x_test, y_test)
+
+    # generate NNoM model, x_test is the calibration dataset used in quantisation process
+    nnom.generate_model(model, x_test, format='hwc', per_channel_quant=False, name=out_path)
 
 
-print('Wrote C code to', out_path)
+def main():
+
+    print('Keras version', keras.__version__)
+
+    epochs = 10
+    num_classes = 10
+    train_samples = None # can be used to limit
+
+    # The data, split between train and test sets:
+    (x_train, y_train), (x_test_original, y_test_original) = cifar10.load_data()
+
+    # convert to grayscale
+
+    x_test = x_test_original
+    y_test = y_test_original
+    print(x_train.shape[0], 'train samples')
+    print(x_test.shape[0], 'test samples')
+
+    # Convert class vectors to binary class matrices.
+    y_train = tf.keras.utils.to_categorical(y_train, num_classes)
+    y_test = tf.keras.utils.to_categorical(y_test, num_classes)
+
+    # reshape to 4 d becaue we build for 4d?
+    #x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1)
+    #x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)
+    print('x_train shape:', x_train.shape)
+
+    # quantize the range to q7
+    x_test = x_test.astype('float32')/255
+    x_train = x_train.astype('float32')/255
+    print("data range", x_test.min(), x_test.max())
+
+    # XXX: make training faster
+    if train_samples is not None:
+        x_train = x_train[:train_samples]
+        y_train = y_train[:train_samples]
+
+    #build model
+    s = x_test.shape[1:]
+    model = build_model(width=s[0], height=s[1], classes=num_classes, channels=3)
+
+    model.summary()
+
+    # train model
+    history = train(model, x_train, y_train, x_test.copy(), y_test.copy(), epochs=epochs)
+
+    save_model(model, save_dir)
+    del model
+    tf.keras.backend.clear_session()
+
+    # convert
+    generate_c_model(x_test, y_test, save_dir)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
