34476a1be9f0c12625a2fbf373a1c2ce147f3691
Jon Nordby <jononor@gmail.com>
2024-05-17 02:56:22 +0200

    openml-cc18: Fixup k means clustering
    Seems to give quite little impact

--- handson/openml-18/analyze.py
+++ handson/openml-18/analyze.py
@@ -6,7 +6,7 @@ df = pandas.read_parquet('out.parquet')
 print(df)
 
 
-g = seaborn.catplot(data=df, kind='box', y='test_score', x='dataset', hue='experiment')
+g = seaborn.catplot(data=df, kind='box', y='test_roc_auc', x='dataset', hue='experiment')
 g.set(ylim=(0.90, 1.0))
 
 g.figure.savefig('analyze.png')
--- handson/openml-18/run_experiments.py
+++ handson/openml-18/run_experiments.py
@@ -9,6 +9,7 @@ from sklearn.preprocessing import RobustScaler, FunctionTransformer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_validate
 from sklearn.cluster import KMeans
+from sklearn.base import BaseEstimator, ClassifierMixin
 
 import pandas
 import numpy
@@ -40,7 +41,7 @@ def tree_nodes(model, a=None, b=None):
     """
     Number of nodes total
     """
-    model = model.named_steps['customrandomforestclassifier']
+    model = model.named_steps['customrandomforestclassifier'].estimator
 
     trees = get_tree_estimators(model)
     nodes = [ len(e.tree_.children_left) for e in trees ]
@@ -48,73 +49,106 @@ def tree_nodes(model, a=None, b=None):
 
 def tree_leaves(model, a=None, b=None):
     """
-    Average depth of model
     """
-    model = model.named_steps['customrandomforestclassifier']
+    model = model.named_steps['customrandomforestclassifier'].estimator
 
     trees = get_tree_estimators(model)
     leaves = [ numpy.count_nonzero((e.tree_.children_left == -1) & (e.tree_.children_right == -1)) for e in trees ]
     return numpy.sum(leaves)
 
+def unique_leaves(model, a=None, b=None):
+    """
+    """
+    model = model.named_steps['customrandomforestclassifier'].estimator
+
+    trees = get_tree_estimators(model)
+
+    ll = []
+    for e in trees:
+        l = e.tree_.value[(e.tree_.children_left == -1) & (e.tree_.children_right == -1)]
+        ll.append(l)
+    leaves = numpy.squeeze(numpy.concatenate(ll))
+
+    return len(numpy.unique(leaves, axis=0))
+
 def leaf_size(model, a=None, b=None):
     """
-    Average depth of model
+    Average size of leaves
     """
-    model = model.named_steps['customrandomforestclassifier']
+    model = model.named_steps['customrandomforestclassifier'].estimator
 
     trees = get_tree_estimators(model)
     sizes = [ e.tree_.value[(e.tree_.children_left == -1) & (e.tree_.children_right == -1)].shape[-1] for e in trees ]
     return numpy.median(sizes)
 
-class CustomRandomForestClassifier(RandomForestClassifier):
+class CustomRandomForestClassifier(BaseEstimator, ClassifierMixin):
 
-    def __init__(self, clusters=100, **kwargs):
-        # FIXME: parameters passing does not work
-        print('INIT', kwargs)
-        super().__init__(**kwargs)
+    def __init__(self, clusters=None, **kwargs):
+        #print('INIT', kwargs)
 
+        self.estimator = RandomForestClassifier(**kwargs)
         self.clusters = clusters
 
     def get_params(self, deep=True):
-        return super().get_params()    
+        return self.estimator.get_params()    
 
     def set_params(self, **parameters):
-        print("SET", **parameters)
-        return super().set_params(**parameters)
+        #print("SET", **parameters)
+        return self.estimator.set_params(**parameters)
+
+    def predict(self, X):
+        return self.estimator.predict(X)
+
+    def predict_proba(self, X):
+        return self.estimator.predict_proba(X)
 
     def fit(self, X, y):
-        ret = super().fit(X, y)
+        ret = self.estimator.fit(X, y)
 
-        # Find leaves
-        ll = []
-        for e in self.estimators_:
-            is_leaf = (e.tree_.children_left == -1) & (e.tree_.children_right == -1)
-            l = e.tree_.value[is_leaf]
-            ll.append(l)
+        if self.clusters is not None:
 
-        leaves = numpy.concatenate(ll)
-        assert leaves.shape[1] == 1, 'only single output supported'
-        leaves = numpy.squeeze(leaves)
+            n_classes = len(numpy.unique(y))
+            n_samples = len(y)
 
-        # Cluster the leaves
-        cluster = KMeans(n_clusters=self.clusters, tol=1e-4, max_iter=100)
-        cluster.fit(leaves)
+            n_clusters = max(self.clusters, n_classes)
+            n_clusters = min(n_clusters, n_samples)
 
-        # Replace by closest centroid
-        for e in self.estimators_:
+            #print('clusters', n_clusters, n_classes, n_samples, self.clusters)
 
-            is_leaf = (e.tree_.children_left == -1) & (e.tree_.children_right == -1)
-            values = e.tree_.value
-            assert values.shape[1] == 1
-            c_idx = cluster.predict(numpy.squeeze(values))
-            centroids = cluster.cluster_centers_[c_idx]
-            # XXX: is this correct ??
-            v = numpy.where(numpy.expand_dims(is_leaf, -1), centroids, numpy.squeeze(values))
-            v = numpy.reshape(v, values.shape)
+            # Find leaves
+            ll = []
+            for e in self.estimator.estimators_:
+                is_leaf = (e.tree_.children_left == -1) & (e.tree_.children_right == -1)
+                l = e.tree_.value[is_leaf]
+                ll.append(l)
 
-            for i in range(len(e.tree_.value)):
-                e.tree_.value[i] = v[i]
+            leaves = numpy.concatenate(ll)
+            assert leaves.shape[1] == 1, 'only single output supported'
+            leaves = numpy.squeeze(leaves)
 
+            # Cluster the leaves
+            cluster = KMeans(n_clusters=n_clusters, tol=1e-4, max_iter=100)
+            cluster.fit(leaves)
+
+            # Replace by closest centroid
+            for e in self.estimator.estimators_:
+
+                is_leaf = (e.tree_.children_left == -1) & (e.tree_.children_right == -1)
+                values = e.tree_.value
+                assert values.shape[1] == 1
+                c_idx = cluster.predict(numpy.squeeze(values))
+                centroids = cluster.cluster_centers_[c_idx]
+                #print('SS', centroids.shape)
+                #print('cc', len(numpy.unique(centroids, axis=0)), len(numpy.unique(c_idx)))
+                # XXX: is this correct ??
+                v = numpy.where(numpy.expand_dims(is_leaf, -1), centroids, numpy.squeeze(values))
+                v = numpy.reshape(v, values.shape)
+
+                for i in range(len(e.tree_.value)):
+                    e.tree_.value[i] = v[i]
+
+
+        self.classes_ = self.estimator.classes_
 
         return ret        
 
@@ -136,6 +170,7 @@ def run_dataset(pipeline, dataset_path,
         'nodes': tree_nodes,
         'leaves': tree_leaves,
         'leasize': leaf_size,
+        'uniqueleaves': unique_leaves,
         'roc_auc': 'roc_auc_ovo_weighted',
     }
 
@@ -191,12 +226,12 @@ def main():
     p = make_pipeline(
         RobustScaler(),
         #quantizer,
-        CustomRandomForestClassifier(n_estimators=2, max_depth=5),
+        CustomRandomForestClassifier(n_estimators=10, max_depth=10, clusters=None),
     )
 
     run_id = uuid.uuid4().hex.upper()[0:6]
 
-    out = run_datasets(p, kvs=dict(experiment='rf10'), out_dir='out.parquet', run_id=run_id)
+    out = run_datasets(p, kvs=dict(experiment='rf10_noclust'), out_dir='out.parquet', run_id=run_id)
 
 
 
