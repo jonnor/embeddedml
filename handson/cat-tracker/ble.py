18cf1d48dcb54d5e654b0c41283fb211093d2947
Jon Nordby <jononor@gmail.com>
2024-07-13 17:01:59 +0200

    cat-tracker: Try sleep between BLE
    Overall loop runs.
    But when connections are made, central is not receiving

--- handson/cat-tracker/ble.py
+++ handson/cat-tracker/ble.py
@@ -3,6 +3,7 @@ import sys
 
 from micropython import const
 
+import machine
 import asyncio
 import aioble
 import bluetooth
@@ -21,53 +22,58 @@ _ADV_APPEARANCE_GENERIC_THERMOMETER = const(768)
 _ADV_INTERVAL_MS = 250_000
 
 
-# Register GATT server.
-temp_service = aioble.Service(_ENV_SENSE_UUID)
-temp_characteristic = aioble.Characteristic(
-    temp_service, _ENV_SENSE_TEMP_UUID, read=True, notify=True
-)
-aioble.register_services(temp_service)
+async def send_data(data):
 
+    # Register GATT server.
+    temp_service = aioble.Service(_ENV_SENSE_UUID)
+    temp_characteristic = aioble.Characteristic(
+        temp_service, _ENV_SENSE_TEMP_UUID, read=True, notify=True
+    )
+    aioble.register_services(temp_service)
 
-
-# This would be periodically polling a hardware sensor.
-async def sensor_task():
-    t = 24.5
-    # NOTE: anything over 253 bytes gets silently truncated
-    data = bytearray(250)
-    sequence_no = 0
-    while True:
-
-        struct.pack_into("<h", data, 0, int(sequence_no))
-        struct.pack_into("<h", data, len(data)-2, int(sequence_no))
-        temp_characteristic.write(data, send_update=True)
-
-        print('write new', sequence_no)
-        await asyncio.sleep_ms(100)
-        sequence_no += 1
-
-
-# Serially wait for connections. Don't advertise while a central is
-# connected.
-async def peripheral_task():
-    while True:
-        print('start advertise')
+    print('start advertise')
+    try:
         async with await aioble.advertise(
             _ADV_INTERVAL_MS,
             name="mpy-temp",
             services=[_ENV_SENSE_UUID],
             appearance=_ADV_APPEARANCE_GENERIC_THERMOMETER,
+            timeout_ms=1000,
         ) as connection:
             print("Connection from", connection.device)
-            await connection.disconnected(timeout_ms=None)
+
+            #await connection.disconnected(timeout_ms=None)
+
+            await asyncio.sleep_ms(100) # so data can be transmitted
+            temp_characteristic.write(data, send_update=True)
+            print('wrote data')
+            await asyncio.sleep_ms(500) # so data can be transmitted
+
+    except asyncio.TimeoutError as e:
+        print('connection-timeout', e)
 
 
 # Run both tasks.
 async def main():
-    t1 = asyncio.create_task(sensor_task())
-    t2 = asyncio.create_task(peripheral_task())
-    await asyncio.gather(t1, t2)
+
+    # NOTE: anything over 253 bytes gets silently truncated
+    data = bytearray(250)
+    sequence_no = 0
+
+    while True:
+        print('iter', sequence_no)
+
+        struct.pack_into("<h", data, 0, int(sequence_no))
+        struct.pack_into("<h", data, len(data)-2, int(sequence_no))
+
+        t = asyncio.create_task(send_data(data))
+        await asyncio.gather(t)
+        sequence_no += 1
+
+        aioble.stop()
+        machine.lightsleep(1000)
 
 
 asyncio.run(main())
+
 
