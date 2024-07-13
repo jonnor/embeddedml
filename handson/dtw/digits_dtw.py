1ef3ed7abddcf916c4f85097e34924eccd2e53a8
Jon Nordby <jononor@gmail.com>
2024-07-13 21:22:14 +0200

    dtw: Add other models for comparison
    As a first pass the CNN did not do better,
    suggesting some issues in data preparation

--- handson/dtw/digits_dtw.py
+++ handson/dtw/digits_dtw.py
@@ -23,7 +23,7 @@ def load_digits_dataset(path):
     return out
 
 def compute_features(filenames : pandas.Series, directory : str,
-        mean_normalize=True, variance_normalization=True, drop_zero=True,
+        mean_normalize=True, variance_normalization=True, drop_zero=True, lifter=0,
     ):
 
     def compute_one(f):
@@ -31,7 +31,10 @@ def compute_features(filenames : pandas.Series, directory : str,
         audio, sr = librosa.load(p, sr=None)
         #print('duration', len(audio)/sr)
         assert sr == 8000
-        mfcc = librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=13, n_fft=256)
+        hop_length = 256
+        n_fft = hop_length * 2
+        mfcc = librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=13,
+            n_fft=n_fft, hop_length=hop_length, lifter=lifter)
         if mean_normalize:
             mean = numpy.mean(mfcc, axis=1, keepdims=True)
             mfcc -= mean + 1e-8
@@ -48,6 +51,89 @@ def compute_features(filenames : pandas.Series, directory : str,
     
     return filenames.apply(compute_one)
     
+def plot_features(X, dataset, class_column='digit'):
+
+    n_samples = 10
+
+    from matplotlib import pyplot as plt
+    import librosa.display
+
+    for class_name in dataset[class_column].unique():
+        selected = dataset[dataset[class_column]==class_name].sample(n_samples)
+        print(selected.index)
+        Xs = X[selected.index]
+
+        height = n_samples * 3.0
+        width = 3.0 * 3
+        fig, axs = plt.subplots(n_samples, figsize=(width, height))
+        for ax, x, (idx, sample) in zip(axs, Xs, selected.iterrows()):
+            img = librosa.display.specshow(x.T,
+                    ax=ax,
+                    x_axis='time', 
+                    sr=8000, hop_length=256,
+            )
+            print(sample)
+            ax.set_title(sample['file'])
+
+        fig.tight_layout()
+        fig.savefig(f'features_{class_name}.png')
+        #fig.show()
+
+
+
+from scikeras.wrappers import KerasClassifier
+
+def build_cnn(meta):
+    # meta is a special argument from scikeras
+    n_features_in_ = meta["n_features_in_"]
+    input_shape = meta["X_shape_"][1:]
+    n_classes = meta["n_classes_"]
+
+    from keras import layers, models
+
+    print('input', input_shape, meta["X_shape_"])
+  
+    filters_start = 8
+    dropout = 0.25
+    model = models.Sequential([
+        layers.Input(shape=input_shape),
+        layers.Reshape((input_shape[0], input_shape[1], 1)),
+        layers.Conv2D(filters_start, 3, activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(filters_start*2, 3, activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Dropout(dropout),
+        layers.Flatten(),
+        layers.Dense(32, activation='relu'),
+        layers.Dropout(dropout),
+        layers.Dense(n_classes),
+        layers.Activation("softmax"),
+    ])
+
+    print(model.summary())
+
+    return model
+
+def build_rnn(meta):
+    # meta is a special argument from scikeras
+    n_features_in_ = meta["n_features_in_"]
+    input_shape = meta["X_shape_"][1:]
+    n_classes = meta["n_classes_"]
+
+    from keras import layers, models
+
+    gru_units = 8
+    dropout = 0.25
+    model = models.Sequential([
+        layers.Input(shape=input_shape),
+        layers.GRU(gru_units, return_sequences=False),
+        layers.Dense(n_classes),
+        layers.Activation("softmax"),
+    ])
+
+    print(model.summary())
+
+    return model
 
 def main():
     
@@ -55,7 +141,7 @@ def main():
     dataset = load_digits_dataset(path)
 
     # XXX: scaling from 1k to 2k sample took time from 15 seconds per fold to 50 seconds per fold with KNN
-    dataset = dataset.sample(n=1000, random_state=1)
+    dataset = dataset.sample(n=1000, random_state=1).reset_index()
 
     print(dataset)
     print('groups', dataset.speaker.nunique())
@@ -63,7 +149,9 @@ def main():
     print('classes', dataset.digit.value_counts().sort_index())
 
     recordings_dir = os.path.join(path, 'recordings')
-    features = compute_features(dataset.file, recordings_dir)
+    features = compute_features(dataset.file, recordings_dir,
+        mean_normalize=True, variance_normalization=False, drop_zero=True, lifter=0,
+    )
 
     shapes = [ f.shape for f in features ]
     print('feature-dims',
@@ -78,7 +166,7 @@ def main():
     from sklearn.model_selection import cross_validate
     from sklearn.model_selection import GroupShuffleSplit
 
-    length = 10
+    length = 15
     n_features = features.iloc[0].shape[0]
 
     # standardize sequences to a fixed length
@@ -87,17 +175,38 @@ def main():
         x = numpy.zeros(shape=(length, n_features))
         ff = f.T[0:length, :]
         x[0:len(ff), :] = ff
+
         xx.append(x)
+        #xx.append(numpy.ravel(x))
+
     X = numpy.stack(xx)
 
+    #plot_features(X, dataset)
 
+    #return # XXX:
+    
     # TODO: set metric to be Z-normalized Euclidean?
-    clf = KNeighborsTimeSeriesClassifier(n_neighbors=3,
+    clf = KNeighborsTimeSeriesClassifier(n_neighbors=1,
         metric="dtw",
-        metric_params=dict(global_constraint="sakoe_chiba", sakoe_chiba_radius=2),
+        metric_params=dict(global_constraint="sakoe_chiba", sakoe_chiba_radius=1),
         n_jobs=None,
     )
     
+    from sklearn.ensemble import RandomForestClassifier
+    #clf = RandomForestClassifier(n_estimators=100)
+
+    """
+    from scikeras.wrappers import KerasClassifier
+    clf = KerasClassifier(
+        build_cnn,
+        loss="sparse_categorical_crossentropy",
+        epochs=60,
+        fit__validation_split=0.2, 
+        optimizer__learning_rate=0.001,
+        #hidden_layer_dim=100,
+    )
+    """
+
     splitter = GroupShuffleSplit(n_splits=3, test_size=0.2)
     res = cross_validate(clf, X, dataset.digit,
             cv=splitter, groups=dataset.speaker,
