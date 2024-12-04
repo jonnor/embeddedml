0f0e7aea3758e1ee3e4a9c39e7601081fe869371
Jon Nordby <jononor@gmail.com>
2024-12-04 01:07:19 +0100

    PyDataGlobal2024: Actually read temperature from MPU6886

--- presentations/PyDataGlobal2024/code/temperature_sensor/main.py
+++ presentations/PyDataGlobal2024/code/temperature_sensor/main.py
@@ -1,26 +1,29 @@
 from mqtt_as import MQTTClient, config
 import asyncio
+from mpu6886 import MPU6886
+from machine import I2C
 
 # Local configuration
 config['ssid'] = 'FIXME'  # Optional on ESP8266
 config['wifi_pw'] = 'FIXME'
 config['server'] = 'test.mosquitto.org'
 
+mpu = MPU6886(I2C(0, sda=21, scl=22, freq=100000))
+
 async def main(client):
     print('main-start')
     await client.connect()
     print('connected')
     
-    n = 0
     while True:
-        await asyncio.sleep(5)
-        print('publish-data', n)
-        await client.publish('pydataglobal2024/send', '{}'.format(n), qos = 0)
-        n += 1
+        t = mpu.temperature
+        print('publish-data', t)
+        await client.publish('pydataglobal2024/send', f'{t:.2f}', qos = 0)
+        await asyncio.sleep(30)
 
 MQTTClient.DEBUG = True  # Optional: print diagnostic messages
 client = MQTTClient(config)
 try:
     asyncio.run(main(client))
 finally:
-    client.close()  # Prevent LmacRxBlk:1 errors
+    client.close()
