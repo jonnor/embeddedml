e56865286dec26cb548e1b7817aef40729b07f6a
Jon Nordby <jononor@gmail.com>
2025-07-13 20:39:57 +0200

    luxmeter: Add code for running continously

---
+++ handson/luxmeter/luxmeter_run.py
@@ -0,0 +1,69 @@
+
+
+import time
+import machine
+import array
+
+import npyfile
+import as7343
+
+from luxmeter_core import load_pipeline
+#from mini_encoder_c import MiniEncoderCHat
+
+# I2C on Groove connector of M5StickC PLUS 2
+i2c1 = machine.I2C(id=0, sda=32, scl=33)
+
+# I2C on stick header, for stick Hat
+i2c2 = machine.I2C(id=1, sda=0, scl=26)
+
+def as7343_fifo_level(sensor) -> int:
+    level = sensor.r_uint8(as7343.FIFO_LVL)
+    return level
+
+
+def main():
+    print('main-start')
+
+    # Load model
+    pipeline_path = 'pipeline.npy'
+    n_features = 18
+    scaler, model = load_pipeline(pipeline_path, expect_features=n_features)
+
+
+    # Note: the measurement time is per set of 6 channels
+    # So for 18 channels, effective time is 3x as long
+    mtime = 100
+
+    spectral = as7343.AS7343(i2c1)
+
+    # Use FIFO
+    spectral.start_measurement()
+    spectral.set_integration_time(50*1000) # in us, max 182000 us
+    spectral.set_measurement_time(mtime) # in ms
+
+    spectral.set_illumination_current(60) # mA
+    spectral.set_illumination_led(False)
+
+    while True:
+
+
+        # check for new data
+        level = as7343_fifo_level(spectral)
+        if level >= 18:
+            # TODO: use a pre-allocated array and read into that
+            data = spectral.read_fifo()
+            sample = array.array('f', data)
+
+            start = time.ticks_us()
+            t = scaler.transform(sample)
+            out = model.predict(t)
+            duration = time.ticks_diff(time.ticks_us(), start) / 1000
+            print('predict-time', duration)
+            print('predict', out)
+
+        # wait
+        time.sleep_ms(10)
+
+if __name__ == '__main__':
+    main()
+
