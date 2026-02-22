3c384bfbf4ecd8b639b8bdbcdaa132cc9cdb9cc1
Jon Nordby <jononor@gmail.com>
2026-02-22 22:42:35 +0100

    fpga-riscv: Zephyr based firmware works

--- handson/fpga-riscv/zfirmware/src/main.c
+++ handson/fpga-riscv/zfirmware/src/main.c
@@ -14,17 +14,17 @@ int main(void)
     // Init
     gpio2_out_write(0xFF);
 
-    //gpio2_out_write(0xFE);
-    //busy_wait(200);
+    gpio2_out_write(0xFE);
     k_msleep(200);
     //gpio2_out_write(0xFD);
-    //busy_wait(200);
     k_msleep(200);
 
     while (1) {
         k_msleep(500);
+        gpio2_out_write(0xFF);
 	    printk("Hello!\n");
         k_msleep(500);
+        gpio2_out_write(0xFD);
     }
 
 	return 0;
